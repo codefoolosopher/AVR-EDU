3a50c28426e159dff1297961e4483beba8bd61ef
codefoolosopher <codefoolosopher@gmail.com>
2023-07-10 22:25:41 +0900

    Up to date

--- Basic_C_Lang/00_Essential_Training/1_Data_Types/1_Data_Types/main.c
+++ Basic_C_Lang/00_Essential_Training/1_Data_Types/1_Data_Types/main.c
@@ -10,7 +10,6 @@
 #include <util/delay.h>
 #include <stdio.h>
 #include "uart0_driver.h"
-
 FILE OUTPUT = FDEV_SETUP_STREAM(UART0_tx, NULL, _FDEV_SETUP_WRITE);
 
 int main(void)
@@ -18,16 +17,25 @@ int main(void)
 	UART0_init();
 	stdout = &OUTPUT;
     
+	// Display the size of each data type
 	printf("\n\r");
 	printf("--- Size of data types in ATmega\n\r");
-	printf("%d\n\r", sizeof(char));
-	printf("%d\n\r", sizeof(int));
-	printf("%d\n\r", sizeof(short));
-	printf("%d\n\r", sizeof(long));
-	printf("%d\n\r", sizeof(float));
-	printf("%d\n\r", sizeof(double));
+	printf("char: %d byte\n\r", sizeof(char));
+	printf("int: %d byte\n\r", sizeof(int));
+	printf("short: %d byte\n\r", sizeof(short));
+	printf("long: %d byte\n\r", sizeof(long));
+	printf("float: %d byte\n\r", sizeof(float));
+	printf("double: %d byte\n\r", sizeof(double));
+	printf("--------------------------------\n\r");
 	
+	// Display each value corresponding to each data type
+	char str[6] = "hello";
 	
+	printf("--- Values to each data type----\n\r");
+	printf("Integer: %d\n\r", 128);
+	printf("Float: %f\n\r", 1.28);
+	printf("String: %s\n\r", str);
+	printf("Character: %c\n\r", 'a');
 	
     while (1) 
     { // IDLE
