7e6c4c37782a46f916eebc53d7a0db56c262a4eb
codefoolosopher <codefoolosopher@gmail.com>
2023-07-10 22:36:24 +0900

    Add commnet: how to display floating number with printf on serial terminal

--- Basic_C_Lang/00_Essential_Training/1_Data_Types/1_Data_Types/main.c
+++ Basic_C_Lang/00_Essential_Training/1_Data_Types/1_Data_Types/main.c
@@ -20,16 +20,16 @@ int main(void)
 	// Display the size of each data type
 	printf("\n\r");
 	printf("--- Size of data types in ATmega\n\r");
-	printf("char: %d byte\n\r", sizeof(char));
-	printf("int: %d byte\n\r", sizeof(int));
-	printf("short: %d byte\n\r", sizeof(short));
-	printf("long: %d byte\n\r", sizeof(long));
-	printf("float: %d byte\n\r", sizeof(float));
-	printf("double: %d byte\n\r", sizeof(double));
+	printf("char: %d byte\n\r", sizeof(char)); // 1byte
+	printf("int: %d byte\n\r", sizeof(int)); // 2byte
+	printf("short: %d byte\n\r", sizeof(short)); // 2byte
+	printf("long: %d byte\n\r", sizeof(long)); // 4byte
+	printf("float: %d byte\n\r", sizeof(float)); // 4byte
+	printf("double: %d byte\n\r", sizeof(double)); // 4byte
 	printf("--------------------------------\n\r");
 	
 	// Display each value corresponding to each data type
-	char str[6] = "hello";
+	char str[6] = "hello"; // contain null char
 	
 	printf("--- Values to each data type----\n\r");
 	printf("Integer: %d\n\r", 128);
@@ -43,3 +43,10 @@ int main(void)
 	return 0;
 }
 
+/* Appendix: how to represent floating number in a serial terminal
+ * 1. RMB(Name of Project) -> Project property -> Toolchain -> AVR/GNU Linker -> Libraries
+ * 2. [Add Libraries] -> type "libprintf_flt"
+ * 3. Toolchain -> AVR/GNU Linker -> General
+ * 4. Check "Use vprintf library"
+ * 
+ */ 
