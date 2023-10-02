65ce558aeb5233dc8565e5b0bf33d22ebe87199f
William S. Woo <codefoolosopher@gmail.com>
2023-10-02 22:05:32 +0900

    Update

--- Basic_C_Lang/00_Essential_Training/2_Operator/2_Operator/main.c
+++ Basic_C_Lang/00_Essential_Training/2_Operator/2_Operator/main.c
@@ -20,19 +20,28 @@ int main(void)
 	/* printf enable */
 	stdout = &OUTPUT;
 	
-	float var1, var2, var3, var4;
+	float f_var1, f_var2, f_var3, f_var4;
+	int n_var1 = 3, n_var2 = 5;
 	
-	var1 = 3 / 2; // no type casting
-	var2 = 3.0 / 2;
-	var3 = 3 / 2.0;
-	var4 = 3.0 / 2.0;
+	f_var1 = 3 / 2; // no type casting
+	f_var2 = 3.0 / 2;
+	f_var3 = 3 / 2.0;
+	f_var4 = 3.0 / 2.0;
 	
-	printf("\n\r");
-	printf("Result 1: %f\n\r", var1); // 1.000000
-	printf("Result 2: %f\n\r", var2); // 1.500000
-	printf("Result 3: %f\n\r", var3); // 1.500000
-	printf("Result 4: %f\n\r", var4); // 1.500000
+	printf("\n\rLet'see arithmetic operators\n\r");
+	printf("Result 1: %f\n\r", f_var1); // 1.000000
+	printf("Result 2: %f\n\r", f_var2); // 1.500000
+	printf("Result 3: %f\n\r", f_var3); // 1.500000
+	printf("Result 4: %f\n\r", f_var4); // 1.500000
+	printf("++++++++++++++++++++++++++++++++++++++++\n\r");
 	
+	printf("\n\rLet's see assignment & logical operators\n\r");
+	
+	if ( n_var1 == n_var2) printf("They are same\n\r");
+	else printf("They are not the same\n\r");
+	
+	if ((n_var1 = n_var2)) printf("Result of conditional operation is TRUE\n\r"); // b/c it is not 0(false)
+	printf("n_var1 : %d\n\r", n_var1);
     /* Replace with your application code */
     while (1) 
     {
